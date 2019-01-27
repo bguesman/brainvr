3c74926256111960a50dfab8ff000132cec331c4
Dmitrias <dmitrijs.celinskis@fulbrightmail.org>
2019-01-27 09:17:24 -0500

    Late night changes

--- web-server-test/stream_test.py
+++ web-server-test/stream_test.py
@@ -4,7 +4,7 @@ import scipy
 from scipy import signal
 from matplotlib import pyplot as plt
 
-r = np.empty([1, 4])
+r = np.zeros([1, 4])
 
 def bandpower(x, fs, fmin, fmax):
 	f, Pxx = scipy.signal.periodogram(x, fs=fs)
@@ -12,46 +12,47 @@ def bandpower(x, fs, fmin, fmax):
 	ind_max = scipy.argmax(f > fmax) - 1
 	return scipy.trapz(Pxx[ind_min: ind_max], f[ind_min: ind_max])
 
-def get_bands():
-	global r;
-	return r[r.shape[0] - 1]
+# def get_bands():
+# 	global r
+# 	# return r[r.shape[0] - 1]
+# 	return r.shape[0]
 
-def band():
-	global r;
-	#f = open("data_try.txt","r")
-	#print(f.read())
-	while True:
-		data = pandas.read_csv('test-data.txt', header = 6)
-		d = data.values;
-		ch = np.array(d[-100:,1:9], dtype=np.float32)
+# def band():
+# 	#f = open("data_try.txt","r")
+# 	#print(f.read())
+# 	while True:
+# 		data = pandas.read_csv('‎⁨Macintosh HD⁩/Users⁩/dc⁩/Desktop⁩/SavedData/OpenBCI-RAW-EEG_test.txt', header = 6)
+# 		d = data.values
+# 		ch = np.array(d[-100:,1:9], dtype=np.float32)
 
-		it = 0
-		Ca = 0
-		Ct = 0
-		Cd = 0
-		Cb = 0
+# 		it = 0
+# 		Ca = 0
+# 		Ct = 0
+# 		Cd = 0
+# 		Cb = 0
 
-		for i in range(8):
-			if (np.std(ch[:,i])) > 0:
-				it = it + 1
-				Cd = Cd + bandpower(ch[:,i], 250, 0.1, 4)
-				Ct = Ct + bandpower(ch[:,i], 250, 4, 8)
-				Ca = Ca + bandpower(ch[:,i], 250, 8, 15)
-				Cb = Cb + bandpower(ch[:,i], 250, 15, 30)
+# 		for i in range(8):
+# 			if (np.std(ch[:,i])) > 0:
+# 				it = it + 1
+# 				Cd = Cd + bandpower(ch[:,i], 250, 0.1, 4)
+# 				Ct = Ct + bandpower(ch[:,i], 250, 4, 8)
+# 				Ca = Ca + bandpower(ch[:,i], 250, 8, 15)
+# 				Cb = Cb + bandpower(ch[:,i], 250, 15, 30)
 
-		if it > 0:
-			Ca = Ca/it
-			Ct = Ct/it
-			Cd = Ca/it
-			Cb = Cb/it
-		else:
-			Ca = 0
-			Ct = 0
-			Cd = 0
-			Cb = 0
+# 		if it > 0:
+# 			Ca = Ca/it
+# 			Ct = Ct/it
+# 			Cd = Ca/it
+# 			Cb = Cb/it
+# 		else:
+# 			Ca = 0
+# 			Ct = 0
+# 			Cd = 0
+# 			Cb = 0
 
-		#r = np.array([Ca, Ct, Cd, Cb])
-		r = np.append(r, [Ca, Ct, Cd, Cb])
+# 		#r = np.array([Ca, Ct, Cd, Cb])
+# 		global r
+# 		r = np.vstack((r, [Ca, Ct, Cd, Cb]))
 
 
 
--- web-server-test/test-server.py
+++ web-server-test/test-server.py
@@ -6,7 +6,13 @@ from flask_cors import CORS, cross_origin
 from flask import jsonify
 from random import uniform
 import numpy as np
-from stream_test import band, get_bands
+from stream_test import bandpower
+
+import pandas
+import numpy as np
+import scipy
+from scipy import signal
+from matplotlib import pyplot as plt
 
 
 app = Flask(__name__)
@@ -23,7 +29,37 @@ def index():
 @cross_origin(origin='*', headers=['Content-Type','Authorization'])
 def datarequest():
     # intensities = [str(uniform(0, 1)) for x in range(0, 6)]
-    band_intensities = np.ndarray.tolist(get_bands())
+    # band_intensities = np.ndarray.tolist(get_bands())
+    # print(band_intensities)
+    data = pandas.read_csv("~/Desktop/SavedData/OpenBCI-RAW-EEG_test.txt", header = 6)
+    d = data.values
+    ch = np.array(d[-100:,1:9], dtype=np.float32)
+
+    it = 0
+    Ca = 0
+    Ct = 0
+    Cd = 0
+    Cb = 0
+
+    for i in range(8):
+        if (np.std(ch[:,i])) > 0:
+            it = it + 1
+            Cd = Cd + bandpower(ch[:,i], 250, 0.1, 4)
+            Ct = Ct + bandpower(ch[:,i], 250, 4, 8)
+            Ca = Ca + bandpower(ch[:,i], 250, 8, 15)
+            Cb = Cb + bandpower(ch[:,i], 250, 15, 30)
+
+    if it > 0:
+        Ca = Ca/it
+        Ct = Ct/it
+        Cd = Ca/it
+        Cb = Cb/it
+    else:
+        Ca = 0
+        Ct = 0
+        Cd = 0
+        Cb = 0
+    band_intensities = [Ca,Ct,Cd,Cb]
     print(band_intensities)
     return jsonify(band_intensities)
 
